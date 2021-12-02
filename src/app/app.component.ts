12d0e11da674e19e987b86330e2d2fb4e34ed645
Madhuri94132 <madhurimys94@gmail.com>
2021-12-02 14:52:53 +0530

    Initial Commit changes..

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Basics';
+  fontColor ="blue";
+  sayHelloId= 2;
+  message="Hello World";
+  canClick=false;
+  canEdit=false;
+  sayMessage(){
+    alert(this.message);
+  }
+}
